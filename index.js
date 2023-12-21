b8aa7f5b99638f77a9c7375613c7bbfbf8dd28e9
mdhossain07 <mrabir8097@gmail.com>
2023-12-21 21:26:48 +0600

    user API implemented

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esabfel.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -20,7 +20,52 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
+
+    const taskCollection = client.db("teamDB").collection("tasks");
+    const userCollection = client.db("teamDB").collection("users");
+
+    app.post("/api/v1/create-user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.post("/api/v1/add-task", async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+
+    app.get("/api/v1/tasks", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    });
+
+    // app.get("/api/v1/get-task", async (req, res) => {
+    //     const {email} = req.params;
+    //     const query = {email: }
+    // })
+
+    app.patch(`/api/v1/update-task/:id`, async (req, res) => {
+      const { id } = req.params;
+      const { status } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedtStatus = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updatedtStatus);
+      res.send(result);
+    });
+
+    app.delete("/api/v1/remove-task/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
